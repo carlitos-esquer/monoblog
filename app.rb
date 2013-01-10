380bd1660889acfdfe5e369797a9060a84c5d27a
Carlos Esquer <carlitos@carlos.(none)>
2013-01-09 19:47:50 -0700

    Initial release

---
+++ app.rb
@@ -0,0 +1,27 @@
+﻿require 'monoriel'
+require 'slim'
+
+class App
+	include Monoriel
+	def index
+		"<h1>Página de inicio</h1>"
+		"<a href='/html/about.html'>Acerca de... </a>"
+	end
+	def blogs
+		"Blogs del usuario"
+	end
+	def usuario
+		"Usuario: Carlitos"
+	end
+	def articulo(id)
+		"<h1>Articulo #{id}<h1><br>"
+		"<h2>Como escribir un Blog</h2><br>"
+		"<p>Información básica del blog </p>"
+	end
+	def eventos
+		slim :eventos
+	end
+	def not_found(*args)
+		slim :not_found
+	end
+end
